158dc2713e2f0143ba39b11792338738cc510dc2
ValentinaLV <tina.lysenok@gmail.com>
2020-04-23 23:08:41 +0300

    changed admin for users app

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from .models import CustomUser
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'created')
     list_filter = ('created', 'updated')
     search_fields = ('username', 'email')
     ordering = ('created',)
     list_per_page = 20
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
